340edcd34c9fe36321050c8c9486e8713ecbec15
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-06-14 14:18:47 +0530

    commit

--- JS-basics/intro/index.js
+++ JS-basics/intro/index.js
@@ -1,1 +1,23 @@
 console.log("blah")
+
+let a=10;
+console.log(a);
+
+{
+    var x=10;
+    let y=4;
+    console.log('x=',x);
+    console.log('y=',y);
+
+}
+// console.log('x=',x);
+// console.log('y=',y);
+
+{
+    const con=4;
+    // con=7;
+    console.log(con);
+}
+// console.log(con)
+a="name";
+console.log(a);
