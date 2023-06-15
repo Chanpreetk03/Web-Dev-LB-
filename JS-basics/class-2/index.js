e8f61e0f3f75d9163587c77bf9ca24b54d140dcc
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-06-15 16:09:32 +0530

    commit

---
+++ JS-basics/class-2/index.js
@@ -0,0 +1,49 @@
+console.log("let's start");
+
+let rectangle={
+    length:1,
+    breadth:2,
+
+    draw: function () {
+        console.log("draw")
+    }
+}
+console.log(rectangle.draw());
+
+
+//factory function
+
+function createReactangle(len,bre){
+    return rectangle={
+        length:len,
+        breadth:bre,
+    
+        draw: function () {
+            console.log("draw");
+        }
+    }
+    
+};
+
+let rectangleObj1=createReactangle(1,2);
+let rectangleObj2=createReactangle(2,2);
+let rectangleObj3=createReactangle(1,5);
+
+
+//constructor function
+
+function Reactangle(len ,bre){
+    this.length=len;
+    this.breadth=bre;
+    this.draw=function(){
+        console.log("drawing")
+    }
+}
+
+let rectObj=new Reactangle(4,6);
+
+rectObj.color='yellow';
+
+delete rectObj.color
+
+//What is the constructor of Rectangle
