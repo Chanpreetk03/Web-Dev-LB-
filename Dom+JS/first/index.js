bda9eea5a2ff70d212b7e5cc42941d887e631f59
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-06-21 13:26:40 +0530

    commit

--- Dom+JS/first/index.js
+++ Dom+JS/first/index.js
@@ -2,16 +2,84 @@
 //     console.log(" i click");
 // });
 
-function eventFunc(){
-    console.log("click clack");
+const { document } = require('postcss')
+
+function eventFunc() {
+	console.log('click clack')
 }
 
-document.addEventListener('click' , eventFunc);
+document.addEventListener('click', eventFunc)
 
 // document.removeEventListener('click' , function(){
 //     console.log("hi");
 // })
 // the above one wont work
 
-document.removeEventListener('click' , eventFunc);
+document.removeEventListener('click', eventFunc)
 //this will
+
+//addEventListner using
+
+const content = document.querySelector('#wrapper')
+content.addEventListener('click', function (event) {
+	console.log(event)
+})
+
+let links = document.querySelectorAll('a')
+let thirdLink = links[2]
+
+//here we stop the default action prevention
+thirdLink.addEventListener('click', function (event) {
+	event.preventDefault()
+	console.log('maza aaya')
+})
+
+// let myDiv=document.createElement('div');
+// for(let i=1 ; i<101 ; i++){
+//     let newElement=document.createElement('p');
+//     newElement.textContent='this is para' + i;
+
+//     newElement.addEventListener('click' , function(event){
+//         console.log('i have clicked on para');
+//     });
+//     myDiv.appendChild(newElement);
+// }
+
+// document.appendChild(myDiv);
+
+// better way:
+
+// function paraStatus(event){
+//     console.log('i have clicked on para');
+// }
+
+// let myDiv=document.createElement('div');
+// for(let i=1 ; i<101 ; i++){
+//     let newElement=document.createElement('p');
+//     newElement.textContent='this is para' + i;
+
+//     newElement.addEventListener('click' ,paraStatus);
+//     myDiv.appendChild(newElement);
+// }
+
+// document.appendChild(myDiv);
+
+// more better way:
+
+function paraStatus(event) {
+	if (event.target.nodeName === 'P') {
+		console.log('i have clicked on para')
+	}
+}
+
+let myDiv = document.createElement('div')
+
+myDiv.addEventListener('click', paraStatus)
+for (let i = 1; i < 101; i++) {
+	let newElement = document.createElement('p')
+	newElement.textContent = 'this is para' + i
+
+	myDiv.appendChild(newElement)
+}
+
+document.appendChild(myDiv)
