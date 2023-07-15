4e37bd1ef377916c9280c6101046f84a4a3dd82a
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-07-16 03:05:50 +0530

    create router

--- React-intermediate/routerapp/src/App.js
+++ React-intermediate/routerapp/src/App.js
@@ -1,10 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Support from './components/Support';
+import NotFound from './components/Not-Found';
+import Labs from './components/Labs';
+import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      app
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+        <Link to="/support">support</Link>
+        </li>
+        <li>
+        <Link to="/about">about</Link>
+        </li>
+        <li>
+        <Link to="/labs">labs</Link>
+        </li>
+        <li>
+        <Link to="*">not found</Link>
+        </li>
+      </ul>
+    </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/support" element={<Support/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/labs" element={<Labs/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </div>
   );
 }
